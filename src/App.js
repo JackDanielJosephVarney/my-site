f044a23dcba9185951710bc2391c52e2506ba588
JackVarney <jack.varney@iosh.com>
2018-04-19 01:29:43 +0100

    homepage no longer takes a child

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
 import HomePage from './containers/home-page/HomePage';
+import Header from './components/header/Header';
 
 class App extends Component {
   render() {
-    return (
-      <HomePage>
-        <div />
-      </HomePage>
-    );
+    return <HomePage />;
   }
 }
 
