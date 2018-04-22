fb3c97ebca41b01c0b9538503c53f9b9405ae4e6
JackVarney <jack.varney@iosh.com>
2018-04-22 17:31:06 +0100

    added sound when button pressed

--- src/containers/home-page/ColorButtons.jsx
+++ src/containers/home-page/ColorButtons.jsx
@@ -4,30 +4,26 @@ import ColorButton from '../../components/color-button/ColorButton';
 import { HomeContext } from '../../state/HomeProvider';
 import { canvasColors } from '../../constants/canvas-colors';
 
+const twelthRootOfTwo = 2 ** (1 / 12);
+
 export default class ColorButtons extends React.Component {
   state = {
     purple: {
-      key: 1,
       className: 'color-button-purple'
     },
     orange: {
-      key: 2,
       className: 'color-button-orange'
     },
     pink: {
-      key: 3,
       className: 'color-button-pink'
     },
     red: {
-      key: 4,
       className: 'color-button-red'
     },
     green: {
-      key: 5,
       className: 'color-button-green'
     },
     blue: {
-      key: 6,
       className: 'color-button-blue'
     }
   };
@@ -35,38 +31,23 @@ export default class ColorButtons extends React.Component {
   order = [1, 2, 3, 4, 5, 6];
   seq = [];
 
+  // audioContext = new AudioContext();
+  audioContext = new (window.AudioContext || window.webkitAudioContext)();
+
   render() {
     return (
       <HomeContext.Consumer>
         {context => (
           <div className="color-buttons-container">
             <div className="color-buttons-row">
-              <ColorButton
-                className={this.state.purple.className}
-                onClick={this.getClickEvent(context, canvasColors.purple, this.state.purple.key)}
-              />
-              <ColorButton
-                className={this.state.orange.className}
-                onClick={this.getClickEvent(context, canvasColors.orange, this.state.orange.key)}
-              />
-              <ColorButton
-                className={this.state.pink.className}
-                onClick={this.getClickEvent(context, canvasColors.pink, this.state.pink.key)}
-              />
+              <ColorButton className={this.state.purple.className} onClick={this.getClickEvent(context, canvasColors.purple, 1, 523.25)} />
+              <ColorButton className={this.state.orange.className} onClick={this.getClickEvent(context, canvasColors.orange, 2, 659.25)} />
+              <ColorButton className={this.state.pink.className} onClick={this.getClickEvent(context, canvasColors.pink, 3, 783.99)} />
             </div>
             <div className="color-buttons-row">
-              <ColorButton
-                className={this.state.red.className}
-                onClick={this.getClickEvent(context, canvasColors.red, this.state.red.key)}
-              />
-              <ColorButton
-                className={this.state.green.className}
-                onClick={this.getClickEvent(context, canvasColors.green, this.state.green.key)}
-              />
-              <ColorButton
-                className={this.state.blue.className}
-                onClick={this.getClickEvent(context, canvasColors.blue, this.state.blue.key)}
-              />
+              <ColorButton className={this.state.red.className} onClick={this.getClickEvent(context, canvasColors.red, 4, 493.88)} />
+              <ColorButton className={this.state.green.className} onClick={this.getClickEvent(context, canvasColors.green, 5, 587.33)} />
+              <ColorButton className={this.state.blue.className} onClick={this.getClickEvent(context, canvasColors.blue, 6, 698.46)} />
             </div>
           </div>
         )}
@@ -74,9 +55,10 @@ export default class ColorButtons extends React.Component {
     );
   }
 
-  getClickEvent = (ctx, color, num) => () => {
+  getClickEvent = (ctx, color, index, freq) => () => {
     ctx.changeColor(color);
-    this.checkClickSequence(num);
+    this.playDing(freq);
+    this.checkClickSequence(index);
 
     if (this.seq.length === 6) {
       ctx.toggleBarcode(false);
@@ -84,10 +66,49 @@ export default class ColorButtons extends React.Component {
     } else ctx.toggleBarcode(true);
   };
 
-  checkClickSequence(num) {
-    const isCorrect = this.order[this.seq.length] === num;
+  checkClickSequence(index) {
+    const isCorrect = this.order[this.seq.length] === index;
 
-    if (isCorrect) this.seq.push(num);
+    if (isCorrect) this.seq.push(index);
     else this.seq = [];
+  }
+
+  playDing(freq) {
+    const now = this.audioContext.currentTime;
+    const { oscNode, gainNode } = this.getAudio(freq, now);
+
+    gainNode.gain.setValueAtTime(0, now);
+    gainNode.gain.linearRampToValueAtTime(0.02, now + 0.001);
+    gainNode.gain.linearRampToValueAtTime(0.003, now + 0.5);
+    gainNode.gain.linearRampToValueAtTime(0, now + 1);
+
+    oscNode.start();
+  }
+
+  getAudio(freq, now) {
+    const oscNode = this.getOscNode();
+    const gainNode = this.getGainNode();
+
+    oscNode.frequency.setValueAtTime(freq, now);
+    gainNode.connect(this.audioContext.destination);
+    oscNode.connect(gainNode);
+
+    return { gainNode, oscNode };
+  }
+
+  getOscNode() {
+    const oscNode = this.audioContext.createOscillator();
+
+    oscNode.type = 'square';
+
+    return oscNode;
+  }
+
+  getGainNode() {
+    const gainNode = this.audioContext.createGain();
+
+    gainNode.connect(this.audioContext.destination);
+
+    return gainNode;
   }
 }
